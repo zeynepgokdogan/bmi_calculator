5043ade8082a727a00684f37ee43b2845a504e72
zeyn03 <z.gokd@icloud.com>
2023-08-26 21:24:08 +0300

    Update homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+enum Gender {
+  male,
+  female,
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -7,43 +12,59 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  late Gender selectedGender;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 4, 9, 54),
-
       appBar: AppBar(
-        centerTitle: true,
-        title: Text("BMI Calculator", style: TextStyle(color: Colors.white),),
-        backgroundColor: Color.fromARGB(255, 24, 37, 110)
-      ),
+          centerTitle: true,
+          title: Text(
+            "BMI Calculator",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color.fromARGB(255, 24, 37, 110)),
+      body: Column(children: [
 
-      body: Column(
-        children: [
-          Row(
-            children: [
-               
-              Padding(padding:EdgeInsets.all(20)),    
+        Padding(padding: EdgeInsets.all(40)),
 
-              ElevatedButton.icon(onPressed: () {},
-               icon: Icon(Icons.female) ,
-               label: Text('FEMALE'),
-               ),
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(20)),
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  selectedGender = Gender.female;});
+              },
+              icon: Icon(Icons.female),
+              label: Text('FEMALE'),
+              ),
+            ),
 
-               Padding(padding:EdgeInsets.all(40)),
+            Padding(padding: EdgeInsets.all(40)),
 
-               ElevatedButton.icon(onPressed: () {},
-               icon: Icon(Icons.male) ,
-               label: Text('MALE'),
-               )
-            ],
-          )
-        ]
-      ),
-      
+            SizedBox(
+              height: 100,
+              width: 100,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                  selectedGender = Gender.male;});
+                },
+                icon: Icon(Icons.male),
+                label: Text('MALE'),
+              ),
+            ),
+          ],
+        ),
 
+        
+      ]),
     );
   }
-  
 }
