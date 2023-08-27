26d62156164b51ed0ed8a2d9923b67212978ca55
zeyn03 <z.gokd@icloud.com>
2023-08-27 21:54:27 +0300

    roundıconbutton class added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'RoundIconButton.dart';
+
 enum Gender {
   male,
   female,
@@ -14,18 +16,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Gender selectedGender;
-  
+  int weight = 20;
+  int age = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 4, 9, 54),
+
       appBar: AppBar(
           centerTitle: true,
           title: Text(
             "BMI Calculator",
             style: TextStyle(color: Colors.white),
           ),
-          backgroundColor: Color.fromARGB(255, 24, 37, 110)),
+          backgroundColor: Color.fromARGB(255, 24, 37, 110),
+          ),
+
       body: Column(children: [
 
         Padding(padding: EdgeInsets.all(40)),
@@ -33,37 +40,91 @@ class _HomePageState extends State<HomePage> {
         Row(
           children: [
             Padding(padding: EdgeInsets.all(20)),
-            SizedBox(
+            Container(
               height: 100,
-              width: 100,
-              child: ElevatedButton.icon(
-              onPressed: () {
-                setState(() {
-                  selectedGender = Gender.female;});
-              },
-              icon: Icon(Icons.female),
-              label: Text('FEMALE'),
-              ),
-            ),
-
-            Padding(padding: EdgeInsets.all(40)),
-
-            SizedBox(
-              height: 100,
-              width: 100,
+              width: 120,
               child: ElevatedButton.icon(
                 onPressed: () {
                   setState(() {
-                  selectedGender = Gender.male;});
+                    selectedGender = Gender.female;
+                  });
+                },
+                icon: Icon(Icons.female),
+                label: Text('FEMALE'),
+                //style: ButtonStyle(backgroundColor: Color.fromARGB(255, 24, 37, 110),),
+              ),
+            ),
+   
+            Padding(padding: EdgeInsets.all(20)),
+
+            Container(
+              height: 100,
+              width: 120,
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    selectedGender = Gender.male;
+                  });
                 },
                 icon: Icon(Icons.male),
                 label: Text('MALE'),
+                //style: ButtonStyle(backgroundColor: Color.fromARGB(255, 24, 37, 110),),
+                
               ),
             ),
           ],
         ),
 
         
+
+
+        Row(
+          children: [
+            Padding(padding: EdgeInsets.all(20)),
+            Container(
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 24, 37, 110),
+              child: 
+               Row(
+                 children: [
+                   Column(
+                      children: [
+                        Padding(padding: EdgeInsets.all(10)),
+                        Text('WEIGHT', style: TextStyle(color: Colors.white),), 
+                        Text(weight.toString(), style: TextStyle(color: Colors.white),),
+                       
+                        ],
+                    ),      
+                 ],
+               ),
+              ),
+
+              Padding(padding: EdgeInsets.all(20)),
+
+              Container(
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 24, 37, 110),
+              child: 
+               Row(
+                 children: [
+                   Column(
+                      children: [
+                        Padding(padding: EdgeInsets.all(10)),
+                        Text('AGE', style: TextStyle(color: Colors.white),), 
+                        Text(weight.toString(), style: TextStyle(color: Colors.white),),
+                       
+                        ],
+                    ),
+                 ],
+               ),
+              ),
+
+          ],
+        ),  
+
+
       ]),
     );
   }
